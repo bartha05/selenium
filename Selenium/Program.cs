330b82ff488ee69bda5a8019aa96cc6233605625
bartha05 <bartha05@gmail.com>
2019-05-21 13:18:16 +0200

    Add files via upload

---
+++ Selenium/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace FirstSeleniumTest
+{
+    class Program
+    {
+         static void Main(string[] args)
+        
+        {
+           IWebDriver driver = new ChromeDriver(@"C:\Users\uq92kh\src\Selenium");
+           
+           {
+             driver.Navigate().GoToUrl("http://www.google.com");
+             driver.Manage().Window.FullScreen();
+           }
+         }
+
+    }
+}
---
+++ Selenium/test/NND_UI.feature
@@ -0,0 +1,15 @@
+Feature: Login_To_NND
+
+Scenario: Login_To_NND_UAT
+    Given I navigate to "https://direkt-acc.nn.hu" web page 
+    And I login with following test credentials 
+    Examples:
+    |User	 |Password  |SMS code|
+    |01111111|Password01|00000000|
+
+    
+
+
+
+
+    
